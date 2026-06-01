e02d9d4e81f24976ced913f2e20d7e208cb1ea40
Happy_Diogenes <tibu10@naver.com>
2026-06-01 20:26:56 +0900

    프로젝트 초기 환경 구축

---
+++ src/main/java/com/example/philosophycommunity/PhilosophycommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.philosophycommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilosophycommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilosophycommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/philosophycommunity/PhilosophycommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.philosophycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilosophycommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
